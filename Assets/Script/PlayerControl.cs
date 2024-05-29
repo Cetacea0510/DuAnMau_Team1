435dfc8a79640259914293cd74ffc4068f022169
Cetacea0510 <tranglinhongyu@gmail.com>
2024-05-29 10:42:47 +0700

    .

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,9 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    // vận tốc chuyển động 
+    // public, private, protected, internal, protected internal
+    [SerializeField]
+    private float moveSpeed = 5f; // 5m/s
+                                  // Start is called before the first frame update
+                                  // hàm chạy 1 lần duy nhất khi game bắt đầu
+                                  // dùng để khởi tạo giá trị
+
+    // biến kiểm tra hướng di chuyển
+    [SerializeField]
+    private bool _isMovingRight = true;
+
+    // tham chiếu đến rigidbody2D
+    private Rigidbody2D _rigidbody2D;
+    // giá trị của lực nhảy
+    [SerializeField]
+    private float _jumpForce = 20f;
+
+    // tham chiếu đến collider2D
+    private CapsuleCollider2D _capsuleCollider2D;
+
+    //tham chieu den animator
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
