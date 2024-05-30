bf25f3de10b5779c2a166d590a8f632d58931709
Cetacea0510 <tranglinhongyu@gmail.com>
2024-05-30 08:46:05 +0700

    .

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -30,12 +30,37 @@ public class PlayerControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    private void Move()
+    {
+        // left, right, a, d
+        var horizontalInput = Input.GetAxis("Horizontal");
+        // 0: không nhấn, âm: trái, dương: phải
+        // điều khiển phải trái
+        // x=1.5 ----> x=1.5+1=2.5
+        transform.localPosition += new Vector3(horizontalInput, 0, 0)
+            * moveSpeed * Time.deltaTime;
+        // localPosition: vị trí tương đối so với cha
+        // position: vị trí tuyệt đối so với thế giới
+        if (horizontalInput > 0)
+        {
+            _isMovingRight = true;
+        }
+        else if (horizontalInput < 0)
+        {
+            _isMovingRight = false;
+        }
+        // xoay nhân vật
+        transform.localScale = _isMovingRight ?
+            new Vector2(1.406425f, 1.172423f)
+            : new Vector2(-1.406425f, 1.172423f);
     }
 }
