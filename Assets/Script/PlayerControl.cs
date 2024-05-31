c698fd4bdf948895865f08a0e28c3194ada7cdc7
Vominhtan20 <votan.224720@gmail.com>
2024-05-31 19:15:41 +0700

    iu

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -6,61 +6,83 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    public float climbSpeed = 3f;
-    private bool isClimbing = false;
-    private Rigidbody2D rb;
-    public delegate void CoinCollected();
-    public static event CoinCollected OnCoinCollected;
-    [SerializeField] private bool _isMovingRight = true;
-    private Rigidbody2D _rigidbody2D;
-    [SerializeField] private float _jumpForce = 20f;
-    private CapsuleCollider2D _capsuleCollider2D;
-    private Animator _animator;
-    [SerializeField] private TextMeshProUGUI _scoreText;
-    private static int _score = 0;
-    private static int _lives = 3;
-    [SerializeField] private TextMeshProUGUI _livesText;
+    // vận tốc chuyển động 
+    // public, private, protected, internal, protected internal
+    [SerializeField]
+    private float moveSpeed = 5f; // 5m/s
+                                  // Start is called before the first frame update
+                                  // hàm chạy 1 lần duy nhất khi game bắt đầu
+                                  // dùng để khởi tạo giá trị
 
+    // biến kiểm tra hướng di chuyển
+    // Biến kiểm tra xem nhân vật có đang leo thang hay không
+    private bool isClimbing;
+    [SerializeField]
+    private bool _isMovingRight = true;
+
+    // tham chiếu đến rigidbody2D
+    private Rigidbody2D _rigidbody2D;
+    // giá trị của lực nhảy
+    [SerializeField]
+    private float _jumpForce = 20f;
+
+    // tham chiếu đến collider2D
+    private CapsuleCollider2D _capsuleCollider2D;
+
+    //tham chieu den animator
+    private Animator _animator;
+
+    //tham chiếu đên TMP để hiển thị điểm
+    [SerializeField]
+    private TextMeshProUGUI _scoreText;
+    private static int _score = 0;
+
+    private static int _lives = 3;
+    [SerializeField]
+    private TextMeshProUGUI _livesText;
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
         _animator = GetComponent<Animator>();
+        //hiển thị điểm
         _scoreText.text = _score.ToString();
         _livesText.text = _lives.ToString();
     }
 
+    // Update is called once per frame
     void Update()
     {
         Move();
         Jump();
         Climb();
     }
-
     private void Move()
     {
+        //left, right, a,d 
         var horizontalInput = Input.GetAxis("Horizontal");
         transform.localPosition += new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0);
 
         if (horizontalInput > 0)
         {
+            //qua phải 
             _isMovingRight = true;
             _animator.SetBool("isRunning", true);
         }
         else if (horizontalInput < 0)
         {
+            //qua trái
             _isMovingRight = false;
             _animator.SetBool("isRunning", true);
         }
         else
         {
+            //đứng yên
             _animator.SetBool("isRunning", false);
         }
         transform.localScale = _isMovingRight ? new Vector3(1f, 1f, 1f) : new Vector3(-1f, 1f, 1f);
     }
-
     private void Jump()
     {
         var check = _capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Tilemap"));
@@ -74,32 +96,49 @@ public class PlayerControl : MonoBehaviour
             _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ladder"))
         {
             _rigidbody2D.gravityScale = 0;
             isClimbing = true;
-            rb.gravityScale = 0f;
-        }
-        else if (collision.gameObject.CompareTag("Coin"))
-        {
-            HandleCoinCollision(collision.gameObject);
         }
         else
         {
             isClimbing = false;
-            rb.gravityScale = 1f;
+        }
+        //nếu chạm với xu
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            //biến mất xu
+            Destroy(collision.gameObject);
+            //tăng điểm
+            _score += collision.gameObject.GetComponent<Coin>().coinValue;
+            //hiển thị điểm
+            _scoreText.text = _score.ToString();
 
-            if (collision.gameObject.CompareTag("Enemy"))
+        }
+        else if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //nếu va chạm với quái
+            _lives -= 1;
+
+            if (_lives > 0)
             {
-                // Xử lý khi nhân vật chạm vào enemy
-                // Thêm code xử lý ở đây hoặc xóa dòng này nếu không cần thiết
+                //reload game tại màn chơi hiện tại 
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                _livesText.text = _lives.ToString();
+            }
+            else
+            {
+                //dừng game
+                Time.timeScale = 0;
+
             }
         }
     }
 
+    // Hàm mới để xử lý việc leo thang
     private void Climb()
     {
         if (!isClimbing)
@@ -109,28 +148,19 @@ public class PlayerControl : MonoBehaviour
         }
 
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(0, verticalInput * climbSpeed * Time.deltaTime, 0));
 
+        // Di chuyển lên hoặc xuống thang
+        transform.Translate(new Vector3(0, verticalInput * moveSpeed * Time.deltaTime, 0));
+
+        // Cập nhật animation
         if (verticalInput != 0)
         {
             _animator.SetBool("isClimbing", true);
-            _animator.SetFloat("climbSpeed", verticalInput);
+            _animator.SetFloat("climbSpeed", verticalInput); // Set speed của animation leo thang
         }
         else
         {
             _animator.SetBool("isClimbing", false);
-        }
-    }
-
-    private void HandleCoinCollision(GameObject coin)
-    {
-        Destroy(coin);
-        _score += coin.GetComponent<Coin>().coinValue;
-        _scoreText.text = _score.ToString();
-
-        if (OnCoinCollected != null)
-        {
-            OnCoinCollected();
         }
     }
 }
